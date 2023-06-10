1b7508528a35ca794957f04b004cf86619648d4d
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-06-10 19:58:25 +0300

    Add files via upload

--- sources/AscendingIterator.cpp
+++ sources/AscendingIterator.cpp
@@ -1,9 +1,13 @@
 #include "MagicalContainer.hpp"
 
-MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container) : container(container), index(0) {}
-MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container, size_t index) : container(container), index(index) {}
-MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator &other) : container(other.container), index(other.index) {}
+MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container): container(&container), index(0) {}
+MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container, size_t index)
+    : container(&container), index(index) {}
+MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator &other)
+    : container(other.container), index(other.index) {}
 MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator=(const AscendingIterator& other) {
+    if(this->container != other.container)
+        throw std::runtime_error("error");
     if(this != &other) {
         this->container = other.container;
         this->index = other.index;
@@ -11,18 +15,32 @@ MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operat
     return *this;
 }
 
-MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() const{return AscendingIterator(*this);}
-bool MagicalContainer::AscendingIterator::operator>(const AscendingIterator& other) const {return true;}
-bool MagicalContainer::AscendingIterator::operator==(const AscendingIterator& other) const {
-    return this->index == other.index;
+bool MagicalContainer::AscendingIterator::operator<(const AscendingIterator& other_it) const 
+{return this->index < other_it.index;}
+
+bool MagicalContainer::AscendingIterator::operator==(const AscendingIterator& other_it) const 
+{return this->index == other_it.index;}
+
+bool MagicalContainer::AscendingIterator::operator>(const AscendingIterator& other_it) const {
+    return this->index > other_it.index;
 }
-bool MagicalContainer::AscendingIterator::operator<(const AscendingIterator& other) const {return true;}
-int MagicalContainer::AscendingIterator::operator*() {return container.container[index];}
+int MagicalContainer::AscendingIterator::operator*() {
+    return (*container).container[index];
+}
+
+bool MagicalContainer::AscendingIterator::operator!=(const AscendingIterator& other_it) const 
+{return !(*this == other_it);}
+
 MagicalContainer::AscendingIterator& MagicalContainer::AscendingIterator::operator++() {
-    ++index;
+    this->index++;
+    if(index > (*container).container.size())
+        throw std::runtime_error("out of bounds");
     return *this;
 }
-MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() const{
-    return AscendingIterator(this->container, container.size());
+
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() const{
+    return AscendingIterator(*this);
 }
-bool MagicalContainer::AscendingIterator::operator!=(const AscendingIterator& other) const {return !(*this == other);}
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() const{
+    return AscendingIterator((*this->container), (*container).size());
+}
--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -1,28 +1,57 @@
 #pragma once
+
 #include "iostream"
-#include "vector"
 #include "algorithm"
+#include "vector"
 
-using namespace std;
-
-namespace ariel{};
+namespace ariel
+{
+}
 
 class MagicalContainer {
 public:
     std::vector<int> container;
-    std::vector<int> order;
+    std::vector<int*> primes;
 
     MagicalContainer(){}
+    ~MagicalContainer(){for (int* ptr : primes) {
+    delete ptr;
+}}
 
+    size_t size() const;
+    bool isPrime(int);
     void addElement(int);
     void removeElement(int);
-    size_t size() const;
 
+    class PrimeIterator{
+    public:
+
+        MagicalContainer* container;
+        size_t index;
+        int type = 2;
+
+        PrimeIterator(MagicalContainer&);
+        PrimeIterator(MagicalContainer&, size_t);
+        PrimeIterator(const PrimeIterator&);
+        ~PrimeIterator(){}
+
+        PrimeIterator end() const;
+        PrimeIterator begin() const;
+
+        bool operator>(const PrimeIterator&) const;
+        PrimeIterator& operator=(const PrimeIterator&);
+        bool operator==(const PrimeIterator&) const;
+        bool operator!=(const PrimeIterator&) const;
+        int operator*();
+        PrimeIterator& operator++();
+        bool operator<(const PrimeIterator&) const;
+    };
     class SideCrossIterator{
         public:
 
-        MagicalContainer &container;
+        MagicalContainer* container;
         size_t index;
+        size_t side_index;
 
         SideCrossIterator(MagicalContainer&);
         SideCrossIterator(MagicalContainer&, size_t);
@@ -32,43 +61,21 @@ public:
         SideCrossIterator begin() const;
         SideCrossIterator end() const;
 
+        bool operator>(const SideCrossIterator&) const;
         SideCrossIterator& operator=(const SideCrossIterator&);
         bool operator==(const SideCrossIterator&) const;
         bool operator!=(const SideCrossIterator&) const;
-        bool operator<(const SideCrossIterator&) const;
-        bool operator>(const SideCrossIterator&) const;
         int operator*();
         SideCrossIterator& operator++();
+        bool operator<(const SideCrossIterator&) const;
     };
-    
-    class PrimeIterator{
-    public:
 
-        MagicalContainer &container;
-        size_t index;
-
-        PrimeIterator(MagicalContainer&);
-        PrimeIterator(MagicalContainer &, size_t);
-        PrimeIterator(const PrimeIterator&);
-        ~PrimeIterator(){}
-
-        PrimeIterator begin() const;
-        PrimeIterator end() const;
-
-        PrimeIterator& operator=(const PrimeIterator&);
-        bool operator==(const PrimeIterator&) const;
-        bool operator!=(const PrimeIterator&) const;
-        bool operator<(const PrimeIterator&) const;
-        bool operator>(const PrimeIterator&) const;
-        int operator*();
-        PrimeIterator& operator++();
-    };
-    
     class AscendingIterator{
-    public:
+        public:
 
-        MagicalContainer &container;
+        MagicalContainer*container;
         size_t index;
+        int type = 1;
 
         AscendingIterator(MagicalContainer&);
         AscendingIterator(MagicalContainer&, size_t);
@@ -78,13 +85,13 @@ public:
         AscendingIterator begin() const;
         AscendingIterator end() const;
 
-        AscendingIterator& operator=(const AscendingIterator& );
+        bool operator>(const AscendingIterator&) const;
+        AscendingIterator& operator=(const AscendingIterator&);
         bool operator==(const AscendingIterator&) const;
         bool operator!=(const AscendingIterator&) const;
-        bool operator<(const AscendingIterator&) const;
-        bool operator>(const AscendingIterator&) const;
         int operator*();
         AscendingIterator& operator++();
+        bool operator<(const AscendingIterator&) const;
 
     };
 };
